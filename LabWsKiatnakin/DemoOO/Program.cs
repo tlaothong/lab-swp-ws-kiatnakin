ddc746eef41d1497fdebe74964677978d4ac5b35
Teerachai <teerachai@perfenterprise.com>
2016-10-17 13:00:54 +0700

    Add DemoOO.

---
+++ LabWsKiatnakin/DemoOO/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoOO
+{
+    public class BankAccount
+    {
+        private double balance;
+
+        public double Deposit(double amount)
+        {
+            balance += amount;
+            return amount;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var acc = new BankAccount();
+            var r = acc.Deposit(200);
+            Console.WriteLine(r);
+        }
+    }
+}
