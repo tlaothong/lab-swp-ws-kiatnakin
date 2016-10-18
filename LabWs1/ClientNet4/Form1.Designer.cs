7119bcb79b976267e2ee8643ce19b19b68347e7c
Teerachai <teerachai@perfenterprise.com>
2016-10-18 14:40:47 +0700

    Add ClientNet4 for calling delayed service.

---
+++ LabWs1/ClientNet4/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ClientNet4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.NumberTextBox = new System.Windows.Forms.TextBox();
+            this.Add200Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number";
+            // 
+            // NumberTextBox
+            // 
+            this.NumberTextBox.Location = new System.Drawing.Point(134, 46);
+            this.NumberTextBox.Name = "NumberTextBox";
+            this.NumberTextBox.Size = new System.Drawing.Size(234, 26);
+            this.NumberTextBox.TabIndex = 1;
+            // 
+            // Add200Button
+            // 
+            this.Add200Button.AutoSize = true;
+            this.Add200Button.Location = new System.Drawing.Point(134, 115);
+            this.Add200Button.Name = "Add200Button";
+            this.Add200Button.Size = new System.Drawing.Size(79, 30);
+            this.Add200Button.TabIndex = 2;
+            this.Add200Button.Text = "Add 200";
+            this.Add200Button.UseVisualStyleBackColor = true;
+            this.Add200Button.Click += new System.EventHandler(this.Add200Button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 244);
+            this.Controls.Add(this.Add200Button);
+            this.Controls.Add(this.NumberTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox NumberTextBox;
+        private System.Windows.Forms.Button Add200Button;
+    }
+}
+
---
+++ LabWs1/ClientNet4/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ClientNet4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Add200Button_Click(object sender, EventArgs e)
+        {
+            var svc = new FirstWcfSvc.FirstWcfServiceClient();
+            var number = Convert.ToInt32(NumberTextBox.Text);
+            var result = svc.Add200(number);
+            MessageBox.Show(result);
+        }
+    }
+}
