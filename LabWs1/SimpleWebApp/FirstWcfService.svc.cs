a2de939e9b4d5542fc61832a42fe777b0802788f
Teerachai <teerachai@perfenterprise.com>
2016-10-18 13:14:40 +0700

    BROKEN NumToEng WCF.

---
+++ LabWs1/SimpleWebApp/FirstWcfService.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SimpleWebApp
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "FirstWcfService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select FirstWcfService.svc or FirstWcfService.svc.cs at the Solution Explorer and start debugging.
+    public class FirstWcfService : IFirstWcfService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
---
+++ LabWs1/SimpleWebApp/IFirstWcfService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SimpleWebApp
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IFirstWcfService" in both code and config file together.
+    [ServiceContract]
+    public interface IFirstWcfService
+    {
+        [OperationContract]
+        string NumToEng(int number);
+    }
+}
