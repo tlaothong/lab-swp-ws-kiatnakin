88e7fc196be936ffd5d9bf02e31f3e2af58d920b
Teerachai <teerachai@perfenterprise.com>
2016-10-20 14:32:27 +0700

    Complete WinSerHost.
    Signed-off-by: Teerachai <teerachai@perfenterprise.com>

--- Lab4Orchestration/WinSerHost/Service1.cs
+++ Lab4Orchestration/WinSerHost/Service1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Linq;
+using System.ServiceModel;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,12 +18,25 @@ namespace WinSerHost
             InitializeComponent();
         }
 
+        private ServiceHost host;
+
         protected override void OnStart(string[] args)
         {
+            if (host != null)
+            {
+                host.Close();
+            }
+            host = new ServiceHost(typeof(BankAService.BankAService));
+            host.Open();
         }
 
         protected override void OnStop()
         {
+            if (host != null)
+            {
+                host.Close();
+            }
+            host = null;
         }
     }
 }
