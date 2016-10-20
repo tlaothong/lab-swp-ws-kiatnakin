d140dab247fcd3c5fbf0bb58b717050092cf89f1
Teerachai <teerachai@perfenterprise.com>
2016-10-20 11:50:34 +0700

    ConsoleHost

---
+++ Lab4Orchestration/ConsoleHost/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var host = new ServiceHost(typeof(BankBService.BankBService)))
+            {
+                host.Open();
+                Console.WriteLine("Press ENTER to stop service.");
+                Console.ReadLine();
+                //host.Close();
+            }
+        }
+    }
+}
