9e2f9618b491f09e5b463ec44576bd58c21acbdb
Teerachai <teerachai@perfenterprise.com>
2016-10-20 13:22:57 +0700

    Demo Topshelf.

---
+++ Lab4Orchestration/DemoTopshelfService/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace DemoTopshelfService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(hc =>
+            {
+                hc.Service<MyTopshelfService>(shc =>
+                {
+                    shc.ConstructUsing(() => new MyTopshelfService());
+                    shc.WhenStarted(s => s.Start());
+                    shc.WhenStopped(s => s.Stop());
+                });
+                hc.RunAsLocalSystem();
+                hc.SetServiceName("MyTopshelfService");
+                hc.SetDisplayName("MyTopshelf Service");
+                hc.SetDescription("Description");
+            });
+        }
+    }
+
+    public class MyTopshelfService
+    {
+        public void Start()
+        {
+            Console.WriteLine("Service is starting...");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Service is stopping ...");
+        }
+    }
+}
