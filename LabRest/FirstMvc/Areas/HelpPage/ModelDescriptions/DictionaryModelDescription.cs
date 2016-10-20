5f505d2fd7b125a42281f268b009ecc136b3d283
Teerachai <teerachai@perfenterprise.com>
2016-10-20 15:22:14 +0700

    Add LabRest.sln

---
+++ LabRest/FirstMvc/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace FirstMvc.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ LabRest/FirstMvc/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace FirstMvc.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
