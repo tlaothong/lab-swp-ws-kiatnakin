fa3122473ff75c8f8d7782d687a628c9ee40c53f
Teerachai <teerachai@perfenterprise.com>
2016-10-20 16:07:48 +0700

    LabRest

---
+++ LabRest/FirstMvc/Controllers/FirstController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMvc.Controllers
+{
+    public class FirstController : Controller
+    {
+        // GET: First
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: First/Simple1
+        public ActionResult Simple1()
+        {
+            return View();
+        }
+    }
+}
---
+++ LabRest/FirstMvc/Controllers/SecondController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FirstMvc.Controllers
+{
+    public class SecondController : ApiController
+    {
+        // GET: api/Second
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Second/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Second
+        public string Post([FromBody]string value)
+        {
+            return value;
+        }
+
+        // PUT: api/Second/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Second/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
